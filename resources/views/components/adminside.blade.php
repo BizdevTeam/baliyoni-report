eaef6dbddb3c59e63386343a2a1cd9f865cae28d
nioo17 <fernandowisu@gmail.com>
2024-12-20 13:44:50 +0800

    side

--- resources/views/components/adminside.blade.php
+++ resources/views/components/adminside.blade.php
@@ -31,7 +31,7 @@
                 <ul id="dropdown-marketing" class="hidden py-2 pl-8 space-y-2">
                     <li>
                         <a href="{{ route("marketings.rekappenjualan") }}"
-                            class="flex items-center block px-3 py-2 text-gray-700  transition">
+                            class="flex items-center px-3 py-2 text-gray-700  transition">
                             <img src="{{ asset("icon/RekapPenjualan.svg") }}" class="w-5 h-5">
                             <span class="ml-2">Rekap Penjualan</span>
                         </a>
