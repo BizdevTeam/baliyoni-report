400d288374e6b9d33397a4bdf08b6a90faf0d473
ekanesa <craqzh69@gmail.com>
2025-03-26 16:24:07 +0800

    feat: update button styles in multiple views for improved UI consistency and accessibility

--- resources/views/supports/rekappendapatanservisasp.blade.php
+++ resources/views/supports/rekappendapatanservisasp.blade.php
@@ -144,17 +144,15 @@
                             <td class="border border-gray-300 px-4 py-2 text-center">{{ $rekappendapatanservisasp->nilai_pendapatan_formatted }}</td>
                             <td class="border border-gray-300 py-6 text-center flex justify-center gap-2">
                                 <!-- Edit Button -->
-                                <button class="bg-red-600 text-white px-3 py-2 rounded" data-modal-target="#editEventModal{{ $rekappendapatanservisasp->id_rpsasp }}">
+                                <button class="text-red-600 bg-transparent px-3 py-2 rounded" data-modal-target="#editEventModal{{ $rekappendapatanservisasp->id_rpsasp }}">
                                     <i class="fa fa-pen"></i>
-                                    Edit
                                 </button>
                                 <!-- Delete Form -->
                                 <form method="POST" action="{{ route('rekappendapatanservisasp.destroy', $rekappendapatanservisasp->id_rpsasp) }}">
                                     @csrf
                                     @method('DELETE')
-                                    <button class="bg-red-600 text-white px-3 py-2 rounded" onclick="return confirm('Are you sure to delete?')">
+                                    <button class="text-red-600 bg-transparent px-3 py-2 rounded" onclick="return confirm('Are you sure to delete?')">
                                         <i class="fa fa-trash"></i>
-                                        Delete
                                     </button>
                                 </form>
                             </td>
